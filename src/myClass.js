e9aad4b1303af42b4477c890e00a3da00b103b0b
praveenkumarpke <praveenpke@gmail.com>
2022-05-09 16:16:29 +0530

    updated the state property

--- src/myClass.js
+++ src/myClass.js
@@ -4,17 +4,25 @@ class Myclass extends React.Component {
     constructor() {
         super()
         this.state = {
-            fname: 'Praveen',
-            lname: 'Aravind'
-        }
+            fname: '',
+            lname: ''
+        };
+    }
+
+    updateState = () => {
+        this.setState ({
+            fname : 'praveen',
+            lname : 'Aravind'
+        });
     }
 
     render() {
         return (
             <>
                 <div>
-                    <h1>{this.state.fname}</h1>
-                    <p>{this.state.lname}</p>
+                    <h1>firstName : {this.state.fname}</h1>
+                    <p>lastName : {this.state.lname}</p>
+                    <button onClick={this.updateState}>click me!</button>
                 </div>
             </>
         )
